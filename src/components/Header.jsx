096a06bf16230634255fa66bc4a2cd919fb3b128
Diaz Martin <elmartind17@gmail.com>
2025-09-21 23:33:50 -0300

    Hice un nav-bar adaptable para pantallas con menos de 1080px que muestra un menu hamburguesa, en la version de escritorio agregue botones animados con transiciones, tambien una vista previa para la imagen de perfil y los botones del login estan adaptados para ser dinamicos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,12 @@
 //  solamente actualizar diseño... La lógica de reenderizado condicional se debe mantener como está
 
 import Swal from "sweetalert2";
+import "./header.css";
+import { useState } from "react";
 
 export default function Header() {
-  const usuarioAdministrador = false;
-  let usuarioLogueado = false;
+  const usuarioAdministrador = true;
+  let usuarioLogueado = true;
 
   function handleLogin() {
     usuarioLogueado = true;
@@ -18,57 +20,85 @@ export default function Header() {
     console.log(usuarioLogueado);
   }
 
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  const abrir = () => {
+    setMenuAbierto(!menuAbierto);
+  };
+
   return (
-    <header className="">
-      <nav className="">
-        <div className="">
+    <header className="d-flex m-auto position-sticky top-0">
+      <nav className="navbar d-flex">
+        <div className="navbar_logo">
           <h1>Logo</h1>
         </div>
-        <div className="">
-          <nav>
-            <ul className="">
-              <li>
-                {" "}
-                <a href="">Inicio</a>
-              </li>
-              <li>
-                {" "}
-                <a href="">Contacto</a>
-              </li>
-              {/* solo puede ver un usuario administrador */}
-              {usuarioAdministrador ? (
-                <li>
-                  {" "}
-                  <a href="">Admin</a>
-                </li>
-              ) : (
-                <></>
-              )}
-              {usuarioLogueado ? (
-                <>
-                  <li>
-                    <a href="">Cerrar sesión</a>
-                  </li>
-                  <button>
-                    <h2>User</h2>{" "}
-                  </button>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <a href="">Login</a>
-                  </li>
-                  <li>
-                    <a href="">Registro</a>
-                  </li>
-                </>
-              )}
-              <button onClick={handleLogin} className="">
-                Logueo
-              </button>
-            </ul>
-          </nav>
+        <div className="navbar_menu d-flex">
+          <button className="navbar_menu-btn" onClick={abrir}>
+            <img
+              src="https://stremu.netlify.app/icons/menu_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </button>
         </div>
+      </nav>
+      <nav className={menuAbierto ? "mostrar" : "ocultar"}>
+        <ul className="navbar_menu_links list-unstyled d-flex">
+          <li>
+            {" "}
+            <a href="">Inicio</a>
+          </li>
+          <li>
+            {" "}
+            <a href="">Contacto</a>
+          </li>
+
+          {/* solo puede ver un usuario administrador */}
+          {usuarioAdministrador ? (
+            <li>
+              {" "}
+              <a href="">Admin</a>
+            </li>
+          ) : (
+            <></>
+          )}
+
+          <ul className="navbar_menu_btn-container list-unstyled d-flex">
+            {usuarioLogueado ? (
+              <>
+                <li>
+                  <button className="navbar_menu_btn-logout" href="">
+                    Cerrar sesión
+                  </button>
+                </li>
+                <li>
+                  <button className="navbar_menu_btn-user d-flex justify-content-center align-items-center">
+                    <img
+                      src="https://randomuser.me/api/portraits/men/23.jpg"
+                      alt=""
+                    />
+                    {""}
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <button className="navbar_menu_btn-login" href="">
+                    Login
+                  </button>
+                </li>
+                <li>
+                  <button className="navbar_menu_btn-register" href="">
+                    Registro
+                  </button>
+                </li>
+              </>
+            )}
+            <button className="d-none" onClick={handleLogin}>
+              Logueo
+            </button>
+          </ul>
+        </ul>
       </nav>
     </header>
   );
