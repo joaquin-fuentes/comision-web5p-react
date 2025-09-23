6f70e2abd5b50e8733607b2b875f841d2f6af77a
Juan Jose Salvi <salvijuan60@gmail.com>
2025-09-23 15:03:25 -0300

    Se creo la estructura del footer y se les agrego unas animaciones a cada imagen de las redes sociales

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Portada from "./components/home/portada/Portada";
+import Footer from "./components/footer";
 import TablaAlumnos from "./components/admin/TablaAlumnos";
 import TarjetaAlumno from "./components/home/alumnos/TarjetaAlumno";
 import Contador from "./components/contador/Contador";
@@ -83,6 +84,7 @@ export default function App() {
         <br />
         <br />
       </main>
+      <Footer></Footer>
     </>
   );
 }
